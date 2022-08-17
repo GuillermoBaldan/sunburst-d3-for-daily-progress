ee4d724716d0ae4f51c00cc7852118e8b41e2d3e
Guillermo Baldán <43887483+GuillermoBaldan@users.noreply.github.com>
2022-08-17 13:14:19 +0200

    folder, file and code refactor - part 1

--- frontend/script.js
+++ frontend/script.js
@@ -569,15 +569,7 @@ function dateToWeek(date) {
   return result;
 }
 
-//Random Functions
-function getRandomColor() {
-  var letters = "0123456789ABCDEF";
-  var color = "#";
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
+
 
 //Functions for Debugging
 
